1101d78d96f9d896f6ed2422ea22a544ff3a9c6b
bipana <bipanashrestha435@gmail.com>
2023-12-04 10:09:40 +0545

    final

---
+++ Doodle jump/Platform.js
@@ -0,0 +1,9 @@
+class Platform {
+    constructor(x, y, width, height, dy) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.dy = dy;
+    }
+}
