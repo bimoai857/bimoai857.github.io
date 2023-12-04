adf54c3ede4be2df24c71b69597aaecc8a3e50b4
bimoai857 <68723052+bimoai857@users.noreply.github.com>
2023-12-04 10:03:20 +0545

    Delete Doodle Jump directory

--- Doodle Jump/platform.js
+++
@@ -1,9 +0,0 @@
-class Platform {
-    constructor(x, y, width, height, dy) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.dy = dy;
-    }
-}
